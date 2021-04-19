7b89643592171db83022e3a9501c400c9541d1a6
Thomas Shaw <thomas@tomthehuman.com>
2021-04-18 18:40:35 -0700

    Handles string to date conversion Split City, State, Zip string into separate variables for both acct and mail Added appropriate properties for each new variable Connect to DB with INSERT query

--- download-script/upload_lic.py
+++ download-script/upload_lic.py
@@ -1,60 +1,155 @@
+import db
 import csv
+import mysql.connector
+from mysql.connector import Error
+import datetime
 
 with open('./data/report.csv') as datafile:
     data = []
     dataReader = csv.reader(datafile)
     next(dataReader)
+    format_str = '%m/%d/%Y'
     for row in dataReader:
         item = {}
-        item['licNumber'] = row[0]
-        item['statusFrom'] = row[1][:(row[1].find(' '))]
-        item['statusTo'] = row[1][(row[1].find(' ') + 1):]
-        item['licType'] = row[2][:(row[2].find(' ') - 2)]
-        item['issueDate'] = row[3]
-        item['expDate'] = row[4]
+        item['licNumber'] = row[0].strip()
+        item['statusFrom'] = row[1][:(row[1].find(' '))].strip()
+        item['statusTo'] = row[1][(row[1].find(' ') + 1):].strip()
+        item['licType'] = row[2][:(row[2].find(' ') - 2)].strip()
+
+        datetime_issue = datetime.datetime.strptime(row[3].strip(), format_str) if row[3].strip() != '' else None
+        item['issueDate'] = datetime_issue
+
+        datetime_exp = datetime.datetime.strptime(row[4].strip(), format_str) if row[4].strip() != '' else None
+        item['expDate'] = datetime_exp
         
         primOwnAddr = row[5].split('                        ')
         if(primOwnAddr[0][:3] == 'DBA'):
-            item['acctName'] = primOwnAddr[0][5:]
-            item['acctOwn'] = primOwnAddr[1]
-            item['acctStreet'] = primOwnAddr[2][:-1]
-            item['acctCityStateZip'] = primOwnAddr[3]
+            item['acctName'] = primOwnAddr[0][5:].strip()
+            item['acctOwn'] = primOwnAddr[1].strip()
+            item['acctStreet'] = primOwnAddr[2][:-1].strip()
+
+            acctCityStateZip = primOwnAddr[3]
+            item['acctCity'] = acctCityStateZip.split(', ')[0].strip()
+
+            acctStateZip = acctCityStateZip.split(', ')[1]
+            item['acctState'] = acctStateZip[:2].strip()
+            item['acctZip'] = acctStateZip[4:].strip()
         else:
-            item['acctOwn'] = primOwnAddr[0]
-            item['acctStreet'] = primOwnAddr[1][:-1]
-            item['acctCityStateZip'] = primOwnAddr[2]
+            item['acctName'] = ''
+            item['acctOwn'] = primOwnAddr[0].strip()
+            item['acctStreet'] = primOwnAddr[1][:-1].strip()
+            
+            acctCityStateZip = primOwnAddr[2]
+            item['acctCity'] = acctCityStateZip.split(', ')[0].strip()
+
+            acctStateZip = acctCityStateZip.split(', ')[1]
+            item['acctState'] = acctStateZip[:2].strip()
+            item['acctZip'] = acctStateZip[4:].strip()
 
         mailAddr = row[6].split('                              ')
-        print(mailAddr[0])
         if len(mailAddr) > 1:
-            item['mailStreet'] = mailAddr[0]
-            item['mailCityStateZip'] = mailAddr[1]
+            item['mailStreet'] = mailAddr[0].strip()
+
+            mailCityStateZip = mailAddr[1]
+            item['mailCity'] = mailCityStateZip.split(', ')[0].strip()
+
+            mailStateZip = mailCityStateZip.split(', ')[1]
+            item['mailState'] = mailStateZip[:2].strip()
+            item['mailZip'] = mailStateZip[4:].strip()
         elif len(mailAddr) > 0:
-            item['mailStreet'] = mailAddr[0]
-            item['mailCityStateZip'] = ''
+            item['mailStreet'] = mailAddr[0].strip()
+            item['mailCity'] = ''
+            item['mailState'] = ''
+            item['mailZip'] = ''
         else:
             item['mailStreet'] = ''
-            item['mailCityStateZip'] = ''
+            item['mailCity'] = ''
+            item['mailState'] = ''
+            item['mailZip'] = ''
 
         transFromTo = row[7].split('/ ')
         if len(transFromTo) == 0:
             item['transFrom'] = ""
             item['transTo'] = ""
         elif len(transFromTo) == 1:
-            item['transFrom'] = transFromTo[0]
+            item['transFrom'] = transFromTo[0].strip()
             item['transTo'] = ""
         elif len(transFromTo) == 2:
-            item['transFrom'] = transFromTo[0]
-            item['transTo'] = transFromTo[1]
+            item['transFrom'] = transFromTo[0].strip()
+            item['transTo'] = transFromTo[1].strip()
 
         item['geoCode'] = row[11]
         data.append(item)
 
-        print(f"Lic#: {item['licNumber']}, Type: {item['licType']}, Status From: {item['statusFrom']}, Status To: {item['statusTo']}, Issue Date: {item['issueDate']}, Exp. Date: {item['expDate']}, Account Street: {item['acctStreet']}, Account City, State, Zip: {item['acctCityStateZip']}, Mailing Street: {item['mailStreet']}, Mailing City, State, Zip: {item['mailCityStateZip']}, Transfer From: {item['transFrom']}, Transfer To: {item['transTo']}, GeoCode: {item['geoCode']}")
-
+ 
 if __name__ == "__main__":
-    
-    def function():
-        print("Function")
 
-    
+    def upload_data():
+        conn = db.get_db()
+        query = f'''INSERT INTO status_changes(
+            lic_num,
+            status_from,
+            status_to,
+            lic_type,
+            issue_date,
+            exp_date,
+            acct_name,
+            acct_owner,
+            acct_street,
+            acct_city,
+            acct_state,
+            acct_zip,
+            mail_street,
+            mail_city,
+            mail_state,
+            mail_zip,
+            trans_from,
+            trans_to,
+            geocode
+        ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);'''
+
+
+
+        for item in data:
+            args = []
+            args.append(item['licNumber'])
+            args.append(item['statusFrom'])
+            args.append(item['statusTo'])
+            args.append(item['licType'])
+            args.append(item['issueDate'])
+            args.append(item['expDate'])
+            args.append(item['acctName'])
+            args.append(item['acctOwn'])
+            args.append(item['acctStreet'])
+            args.append(item['acctCity'])
+            args.append(item['acctState'])
+            args.append(item['acctZip'])
+            args.append(item['mailStreet'])
+            args.append(item['mailCity'])
+            args.append(item['mailState'])
+            args.append(item['mailZip'])
+            args.append(item['transFrom'])
+            args.append(item['transTo'])
+            args.append(item['geoCode'])
+
+            print('poop')
+
+            try:
+                cursor = conn.cursor()
+                cursor.execute(query, args)
+
+                if cursor.lastrowid:
+                    print('Last Insert ID: ', cursor.lastrowid)
+                else:
+                    print('Last Insert ID Not Found!')
+
+                conn.commit()
+            
+            except Error as e:
+                print(e)
+        
+        print('Upload complete!')
+
+        conn.close()
+
+    upload_data()
