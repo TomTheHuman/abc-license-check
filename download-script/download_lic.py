1ac9ea1ea88311e65c9d08ee157f48fe571ec6d9
Thomas Shaw <thomas@tomthehuman.com>
2021-04-18 18:38:10 -0700

    profile and broswer moved below __name__ = '__main__'

--- download-script/download_lic.py
+++ download-script/download_lic.py
@@ -3,24 +3,22 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
 
 
-profile = webdriver.FirefoxProfile()
-profile.set_preference("broswer.download.folderList", 2)
-profile.set_preference("broswer.download.manager.showWhenStarting", False)
-profile.set_preference("broswer.download.dir", '/home/tomthehuman/Desktop/dev/abc-license-check/download-script/data/')
-
-# Need to figure out why prompt still appears
-profile.set_preference("browser.helperApps.neverAsk.saveToDisk" , "")
-profile.set_preference("browser.helperApps.neverAsk.openFile" , "")
-profile.set_preference("browser.helperApps.alwaysAsk.force", False);
-
-
-
-browser = webdriver.Firefox(firefox_profile=profile)
-browser.get('https://www.abc.ca.gov/licensing/licensing-reports/status-changes/')
-
-
 if __name__ == '__main__':
     
+    profile = webdriver.FirefoxProfile()
+    profile.set_preference("broswer.download.folderList", 2)
+    profile.set_preference("broswer.download.manager.showWhenStarting", False)
+    profile.set_preference("broswer.download.dir", '/home/tomthehuman/Desktop/dev/abc-license-check/download-script/data/')
+
+    # Need to figure out why prompt still appears
+    profile.set_preference("browser.helperApps.neverAsk.saveToDisk" , "")
+    profile.set_preference("browser.helperApps.neverAsk.openFile" , "")
+    profile.set_preference("browser.helperApps.alwaysAsk.force", False);
+
+
+    browser = webdriver.Firefox(firefox_profile=profile)
+    browser.get('https://www.abc.ca.gov/licensing/licensing-reports/status-changes/')
+
     # click radio button
     python_button = browser.find_element(By.XPATH, '//span[text()="Download Report (CSV)"]')
     python_button.click()
