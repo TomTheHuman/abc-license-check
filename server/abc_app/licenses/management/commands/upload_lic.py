621755c5367e057bf28ee5d57a1d4d8c4103d4fd
Thomas Shaw <thomas@tomthehuman.com>
2021-08-05 20:55:58 -0700

    Updated License Upload Script - Issued licenses now useing ORM - IL reports include the status description rather than code - Need to reference stateus by description (find one?)

--- server/abc_app/licenses/management/commands/upload_lic.py
+++ server/abc_app/licenses/management/commands/upload_lic.py
@@ -15,7 +15,7 @@ class Command(BaseCommand):
 
         # Collect data from reports
         self.collect_status_changes()
-        # data_issued_licenses = collect_issued_licenses()
+        self.collect_issued_licenses()
         # data_new_applications = collect_new_applications()
 
         # # Upload data from files to MySQL
@@ -149,123 +149,119 @@ class Command(BaseCommand):
         except IOError:
             print('There was a problem opening report_status_changes.csv. The file may not exist in the data directory.')
 
-    # def collect_issued_licenses():
-    #     '''
-    #     Tries to open report_issued_licenses.csv file from data directory
-    #     Creates list to store dictionaries for each row of data
-    #     Creates dictionaries used to collect formatted data from the report columns
-    #     Strips, slices, and formats data from report to match MySQL data definitions
+    def collect_issued_licenses(self):
+        '''
+        Tries to open report_issued_licenses.csv file from data directory
+        Creates list to store dictionaries for each row of data
+        Creates dictionaries used to collect formatted data from the report columns
+        Strips, slices, and formats data from report to match MySQL data definitions
 
-    #     Returns data_issued_licenses (list of dictionaries referenced above)
+        Returns data_issued_licenses (list of dictionaries referenced above)
 
-    #     If file does not exist, IOError exception is thrown - error message printed
-    #     '''
-    #     try:
-    #         with open('./data/report_issued_licenses.csv') as datafile:
-    #             # Initialize list to store row dicts
-    #             data_issued_licenses = []
+        If file does not exist, IOError exception is thrown - error message printed
+        '''
+        try:
+            with open('./data/report_issued_licenses.csv') as datafile:
+                data_reader = csv.reader(datafile)
+                next(data_reader)
 
-    #             data_reader = csv.reader(datafile)
-    #             next(data_reader)
-
-    #             # String format used in ABC reports            
-    #             format_str = '%m/%d/%Y'
-    #             for row in data_reader:
-    #                 # Initialize new dict to store row data
-    #                 item = {}
-    #                 item['lic_num'] = int(row[0].strip())
-    #                 item['status'] = row[1].strip()
-    #                 item['lic_type'] = int(row[2][:(row[2].find(' '))].strip())
-    #                 item['lic_dup'] = int(row[2][(row[2].find('|') + 1):].strip())
+                # String format used in ABC reports            
+                format_str = '%m/%d/%Y'
+                for row in data_reader:
+                    # Initialize new dict to store row data
+                    issued_license = Report()
+                    issued_license.lic_num = int(row[0].strip())
+                    issued_license.status = row[1].strip()
+                    issued_license.lic_type = int(row[2][:(row[2].find(' '))].strip())
+                    issued_license.lic_dup = int(row[2][(row[2].find('|') + 1):].strip())
                     
-    #                 # Strips datetime string from report and formats using format_str definition above (if column string not empty)
-    #                 # If the above step resulted in a value (not empty string) datetime is converted to MySQL date format, otherwise None value set               
-    #                 datetime_exp = datetime.datetime.strptime(row[3].strip(), format_str) if row[3].strip() != '' and len(row[3]) > 0 else ''
-    #                 item['exp_date'] = datetime_exp.strftime('%Y-%m-%d') if datetime_exp != '' else None
+                    # Strips datetime string from report and formats using format_str definition above (if column string not empty)
+                    # If the above step resulted in a value (not empty string) datetime is converted to MySQL date format, otherwise None value set               
+                    datetime_exp = datetime.datetime.strptime(row[3].strip(), format_str) if row[3].strip() != '' and len(row[3]) > 0 else ''
+                    issued_license.exp_date = datetime_exp.strftime('%Y-%m-%d') if datetime_exp != '' else None
 
-    #                 # Local list variable created to store delimitted values from Primary Owner and Premises Addr. report column
-    #                 # This column will sometimes include the account name (with "DBA: " prefix)
-    #                 # This column will sometimes include the account premises address (if no address, the comma-separator between street and city will remain)
-    #                 # Logic handles all of the above cases and sets omitted values to None where applcable
-    #                 prim_own_addr = row[4].split('                            ')
-    #                 if(prim_own_addr[0][:3] == 'DBA'):
-    #                     item['acct_name'] = prim_own_addr[0][5:].strip()
-    #                     item['acct_own'] = prim_own_addr[1].strip()
+                    # Local list variable created to store delimitted values from Primary Owner and Premises Addr. report column
+                    # This column will sometimes include the account name (with "DBA: " prefix)
+                    # This column will sometimes include the account premises address (if no address, the comma-separator between street and city will remain)
+                    # Logic handles all of the above cases and sets omitted values to None where applcable
+                    prim_own_addr = row[4].split('                            ')
+                    if(prim_own_addr[0][:3] == 'DBA'):
+                        issued_license.acct_name = prim_own_addr[0][5:].strip()
+                        issued_license.acct_own = prim_own_addr[1].strip()
 
-    #                     # If the primary address has been omitted, the report leaves the comma-separator for street and city
-    #                     # This logic will check if there is a single comma (no address info) before continuing
-    #                     if prim_own_addr[2].strip() != ',':
-    #                         item['acct_street'] = prim_own_addr[2][:-1].strip()
+                        # If the primary address has been omitted, the report leaves the comma-separator for street and city
+                        # This logic will check if there is a single comma (no address info) before continuing
+                        if prim_own_addr[2].strip() != ',':
+                            issued_license.acct_street = prim_own_addr[2][:-1].strip()
 
-    #                         acct_cityStateZip = prim_own_addr[3]
-    #                         item['acct_city'] = acct_cityStateZip.split(', ')[0].strip()
+                            acct_cityStateZip = prim_own_addr[3]
+                            issued_license.acct_city = acct_cityStateZip.split(', ')[0].strip()
 
-    #                         acct_stateZip = acct_cityStateZip.split(', ')[1]
-    #                         item['acct_state'] = acct_stateZip[:2].strip()
-    #                         item['acct_zip'] = acct_stateZip[4:].strip()
-    #                     else:
-    #                         item['acct_street'] = None
-    #                         item['acct_city'] = None
-    #                         item['acct_state'] = None
-    #                         item['acct_zip'] = None
-    #                 else:
-    #                     item['acct_name'] = None
-    #                     item['acct_own'] = prim_own_addr[0].strip()
+                            acct_stateZip = acct_cityStateZip.split(', ')[1]
+                            issued_license.acct_state = acct_stateZip[:2].strip()
+                            issued_license.acct_zip = acct_stateZip[4:].strip()
+                        else:
+                            issued_license.acct_street = None
+                            issued_license.acct_city = None
+                            issued_license.acct_state = None
+                            issued_license.acct_zip = None
+                    else:
+                        issued_license.acct_name = None
+                        issued_license.acct_own = prim_own_addr[0].strip()
                         
-    #                     # If the primary address has been omitted, the report leaves the comma-separator for street and city
-    #                     # This logic will check if there is a single comma (no address info) before continuing
-    #                     if prim_own_addr[1].strip() != ',':
-    #                         item['acct_street'] = prim_own_addr[1][:-1].strip()
+                        # If the primary address has been omitted, the report leaves the comma-separator for street and city
+                        # This logic will check if there is a single comma (no address info) before continuing
+                        if prim_own_addr[1].strip() != ',':
+                            issued_license.acct_street = prim_own_addr[1][:-1].strip()
 
-    #                         acct_cityStateZip = prim_own_addr[2]
-    #                         item['acct_city'] = acct_cityStateZip.split(', ')[0].strip()
+                            acct_cityStateZip = prim_own_addr[2]
+                            issued_license.acct_city = acct_cityStateZip.split(', ')[0].strip()
 
-    #                         acct_stateZip = acct_cityStateZip.split(', ')[1]
-    #                         item['acct_state'] = acct_stateZip[:2].strip()
-    #                         item['acct_zip'] = acct_stateZip[4:].strip()
-    #                     else:
-    #                         item['acct_street'] = None
-    #                         item['acct_city'] = None
-    #                         item['acct_state'] = None
-    #                         item['acct_zip'] = None
+                            acct_stateZip = acct_cityStateZip.split(', ')[1]
+                            issued_license.acct_state = acct_stateZip[:2].strip()
+                            issued_license.acct_zip = acct_stateZip[4:].strip()
+                        else:
+                            issued_license.acct_street = None
+                            issued_license.acct_city = None
+                            issued_license.acct_state = None
+                            issued_license.acct_zip = None
 
-    #                 # Local list variable created to store delimitted values from Mailing Addr. report column
-    #                 # This column will either contain the full address string or an empty string
-    #                 # Logic handles the above cases and sets omitted values to None where applicable
-    #                 mail_addr = row[5].split('                              ')
-    #                 if len(mail_addr) > 1:
-    #                     item['mail_street'] = mail_addr[0].strip()
+                    # Local list variable created to store delimitted values from Mailing Addr. report column
+                    # This column will either contain the full address string or an empty string
+                    # Logic handles the above cases and sets omitted values to None where applicable
+                    mail_addr = row[5].split('                              ')
+                    if len(mail_addr) > 1:
+                        issued_license.mail_street = mail_addr[0].strip()
 
-    #                     mail_city_state_zip = mail_addr[1]
-    #                     item['mail_city'] = mail_city_state_zip.split(', ')[0].strip()
+                        mail_city_state_zip = mail_addr[1]
+                        issued_license.mail_city = mail_city_state_zip.split(', ')[0].strip()
 
-    #                     mail_state_zip = mail_city_state_zip.split(', ')[1]
-    #                     item['mail_state'] = mail_state_zip[:2].strip()
-    #                     item['mail_zip'] = mail_state_zip[4:].strip()
-    #                 elif len(mail_addr) > 0:
-    #                     item['mail_street'] = mail_addr[0].strip()
-    #                     item['mail_city'] = None
-    #                     item['mail_state'] = None
-    #                     item['mail_zip'] = None
-    #                 else:
-    #                     item['mail_street'] = None
-    #                     item['mail_city'] = None
-    #                     item['mail_state'] = None
-    #                     item['mail_zip'] = None
+                        mail_state_zip = mail_city_state_zip.split(', ')[1]
+                        issued_license.mail_state = mail_state_zip[:2].strip()
+                        issued_license.mail_zip = mail_state_zip[4:].strip()
+                    elif len(mail_addr) > 0:
+                        issued_license.mail_street = mail_addr[0].strip()
+                        issued_license.mail_city = None
+                        issued_license.mail_state = None
+                        issued_license.mail_zip = None
+                    else:
+                        issued_license.mail_street = None
+                        issued_license.mail_city = None
+                        issued_license.mail_state = None
+                        issued_license.mail_zip = None
 
-    #                 # If each of the columns below are not empty strings, they are stored as dict values
-    #                 # If strings are empty, values are set to None
-    #                 item['action'] = row[6] if len(row[6]) > 0 else None
-    #                 item['conditions'] = row[7] if len(row[7]) > 0 else None
-    #                 item['escrow_addr'] = row[8] if len(row[8]) > 0 else None
-    #                 item['district'] = int(row[9]) if len(row[9]) > 0 else None
-    #                 item['geocode'] = int(row[10]) if len(row[10]) > 0 else None
+                    # If each of the columns below are not empty strings, they are stored as dict values
+                    # If strings are empty, values are set to None
+                    issued_license.action = row[6] if len(row[6]) > 0 else None
+                    issued_license.conditions = row[7] if len(row[7]) > 0 else None
+                    issued_license.escrow_addr = row[8] if len(row[8]) > 0 else None
+                    issued_license.district = int(row[9]) if len(row[9]) > 0 else None
+                    issued_license.geocode = int(row[10]) if len(row[10]) > 0 else None
 
-    #                 data_issued_licenses.append(item)
+                    issued_license.save()
 
-    #             return data_issued_licenses
-    #     except IOError:
-    #         print('There was a problem opening report_issued_licenses.csv. The file may not exist in the data directory.')
+        except IOError:
+            print('There was a problem opening report_issued_licenses.csv. The file may not exist in the data directory.')
 
     # def collect_new_applications():
     #     '''
